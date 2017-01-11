60b7728d0a790e5c0ba8a744d325cc43fe5c3953
German Mikulski <mik.german@gmail.com>
2017-01-11 12:25:48 +0000

    Get request improvement of user view

--- SnippetsPro/controllers/user_controller.php
+++ SnippetsPro/controllers/user_controller.php
@@ -6,13 +6,13 @@
 
     static public function set() {
 
-      
-
     	// read what has to be changed from form
     	$type = $_GET['type'];
       $value = $_GET['value'];
-        
+    
       User::set_data($type, $value);
+
+      require_once("views/change_user_data.php");
 
 
     }
--- SnippetsPro/views/change_user_data.php
+++ SnippetsPro/views/change_user_data.php
@@ -1,9 +1,13 @@
 <div style="width: 300px; height: 150px; border: solid;">
 
-	<form action="../controllers/user_controller.php" method="get">
+	<form action="index.php" method="get">
+
+		<input type="hidden" name="controller" value="user">
+		<input type="hidden" name="action" value="set">
 		<input type="text" name="type">
 		<input type="text" name="value">
-		<input type="submit">
+		<input type="submit" value="Submit">
+
 	</form>
 
 
