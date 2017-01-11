e6977957337ab99da619bc3f85586c1b395e77b8
George Pîrlea <george@dranov.ro>
2017-01-11 15:35:20 +0000

    Add menu

--- SnippetsPro/views/layout.php
+++ SnippetsPro/views/layout.php
@@ -3,6 +3,24 @@
   <head>
   </head>
   <body>
+    <div id="menu">
+      <ul>
+        <li><a href="index.php?controller=home&action=mhome">Home</a></li>
+        <li><a href="index.php?controller=snippet&action=home">All snippets</a></li>
+
+        <?php
+        if(array_key_exists('userID', $_SESSION)) {
+          echo '<li><a href="index.php?controller=snippet&action=home&uid='. $_SESSION['userID'] . '">My snippets</a></li>';
+          echo '<li><a href="index.php?controller=auth&action=logout">Log out</a></li>';
+        }
+        else {
+          echo '<li><a href="index.php?controller=auth&action=loginpage">Log in</a></li>';
+          echo '<li><a href="index.php?controller=auth&action=signuppage">Sign up</a></li>';
+
+        }
+        ?>
+      </ul>
+    </div>
 
     <?php require_once('routes.php'); ?>
 
