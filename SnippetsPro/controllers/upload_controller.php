8110d311e6a8580787aebd11db154480e5ff3aaa
George Pîrlea <george@dranov.ro>
2017-01-11 15:55:28 +0000

    Mkdir uploads/ dir if needed; print pre-format

--- SnippetsPro/controllers/upload_controller.php
+++ SnippetsPro/controllers/upload_controller.php
@@ -1,6 +1,6 @@
 <?php
   class UploadController {
-  
+
     public function viewAll() {
         $files = shell_exec('ls -1 uploads/' . $_SESSION['userID']);
         require_once('views/uploads.php');
@@ -8,16 +8,18 @@
 
     public function upload() {
         $targetDir = 'uploads/' . $_SESSION['userID'] . '/';
-        shell_exec('mkdir uploads/' . $_SESSION['userID']);
+        shell_exec('mkdir -p uploads/' . $_SESSION['userID']);
         $targetFile = $targetDir . $_FILES["myfile"]["name"];
         $uploaded = (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetFile));
         $this->viewAll();
-            
+
     }
-    
+
     public function view() {
+        echo "<pre>";
         require_once('uploads/' . $_SESSION['userID'] . '/' . $_GET['file']);
+        echo "</pre>";
     }
-    
+
   }
 ?>
