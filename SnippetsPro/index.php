e2784deb6f937edbaca357e3d3136202da644327
German Mikulski <mik.german@gmail.com>
2017-01-12 18:05:11 +0000

    ~Deleted error display

--- SnippetsPro/index.php
+++ SnippetsPro/index.php
@@ -2,10 +2,6 @@
   session_start();
   require_once('connection.php');
 
-  ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
-error_reporting(-1);
-
 
   if (isset($_GET['controller']) && isset($_GET['action'])) {
     $controller = $_GET['controller'];
