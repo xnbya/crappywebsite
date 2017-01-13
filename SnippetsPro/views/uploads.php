c54c9352017486977b51b1fd5e424caa48af8e7d
Alexis Enston <xn@outlook.com>
2017-01-13 13:59:25 +0000

    Fix viewall

--- SnippetsPro/views/uploads.php
+++ SnippetsPro/views/uploads.php
@@ -1,6 +1,6 @@
 <h1>Your Uploads </h1>
 <?php
     foreach($lines as $line) {
-        echo('<h4><span class="glyphicon glyphicon-file"></span><a href="download.php?file=' . $line . '">' . $line . '</a></h4>');
+        echo('<h4><a href="download.php?file=' . $line . '">' . $line . '</a></h4>');
     }
  ?>
